4779641f827fbd05e93efa8ffc0a0e6c75a64dd5
L19290641 <146124347+L19290641@users.noreply.github.com>
2024-08-29 16:45:40 +0000

    Ultima actualizacion

---
+++ ejemplo.rb
@@ -0,0 +1,19 @@
+class padre 
+    def initialize(nombre)
+        @nombre = nombre
+    end
+
+    def say_hello
+    "Hola desde #{self}"
+    end
+
+
+class higit jo > padre
+
+    
+
+saluda1 = padre.new ("Darth")
+puts saluda1.say_hello
+
+saluda2 = Hijo.new("Anakin")
+puts saliuda2.say hello
